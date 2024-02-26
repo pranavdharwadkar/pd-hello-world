7c19bebfb0d1e9b32ce1d1fa2b928958a8380510
pranavdharwadkar <pd98@outlook.com>
2024-02-26 01:26:15 +0000

    Changed Page Config

--- Hello.py
+++ Hello.py
@@ -20,11 +20,11 @@ LOGGER = get_logger(__name__)
 
 def run():
     st.set_page_config(
-        page_title="Hello",
+        page_title="Pranav Hello World",
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.sidebar.write("# Welcome to Streamlit! 👋")
 
     st.sidebar.success("Select a demo above.")
 
